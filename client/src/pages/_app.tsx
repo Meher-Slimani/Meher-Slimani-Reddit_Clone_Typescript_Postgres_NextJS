7eb2242875a9904f52c144eae860d226e096654d
Meher Slimani <meher.slimeni@gmail.com>
2021-03-18 17:48:06 +0100

    Add Navbar component

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,22 @@
-import "../styles/globals.css";
+import "../styles/tailwind.css";
 import { AppProps } from "next/app";
 import axios from "axios";
+import { useRouter } from "next/router";
+import Navbar from "../components/Navbar";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 axios.defaults.withCredentials = true;
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
+  return (
+    <>
+      {!authRoute && <Navbar />}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
