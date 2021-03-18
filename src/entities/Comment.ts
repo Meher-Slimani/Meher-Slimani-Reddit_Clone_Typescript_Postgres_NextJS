6e3d8c1bbe348105c8850e6237354743fb69a18c
Meher Slimani <meher.slimeni@gmail.com>
2021-03-18 22:45:41 +0100

    Add Vote entity & vote route

--- src/entities/Comment.ts
+++ src/entities/Comment.ts
@@ -1,3 +1,4 @@
+import { Exclude } from "class-transformer";
 import {
   BeforeInsert,
   Column,
@@ -5,40 +6,54 @@ import {
   Index,
   JoinColumn,
   ManyToOne,
-} from "typeorm"
+  OneToMany,
+} from "typeorm";
 
-import { makeId } from "../util/helpers"
+import { makeId } from "../util/helpers";
 
-import Entity from "./Entity"
-import Post from "./Post"
-import User from "./User"
+import Entity from "./Entity";
+import Post from "./Post";
+import User from "./User";
+import Vote from "./Vote";
 
 @TOEntity("comments")
 export default class Comment extends Entity {
   constructor(comment: Partial<Comment>) {
-    super()
-    Object.assign(this, comment)
+    super();
+    Object.assign(this, comment);
   }
 
   @Index()
   @Column()
-  identifier: string
+  identifier: string;
 
   @Column()
-  body: string
+  body: string;
 
   @Column()
-  username: string
+  username: string;
 
   @ManyToOne(() => User)
   @JoinColumn({ name: "username", referencedColumnName: "username" })
-  user: User
+  user: User;
 
   @ManyToOne(() => Post, (post) => post.comments, { nullable: false })
-  post: Post
+  post: Post;
+
+  @Exclude()
+  @OneToMany(() => Vote, (vote) => vote.comment)
+  votes: Vote[];
+
+  protected userVote: number;
+  setUserVote(user: User) {
+    const index = this.votes?.findIndex(
+      (vote) => vote.username === user.username
+    );
+    this.userVote = index > -1 ? this.votes[index].value : 0;
+  }
 
   @BeforeInsert()
   makeIdAndSlug() {
-    this.identifier = makeId(8)
+    this.identifier = makeId(8);
   }
 }
