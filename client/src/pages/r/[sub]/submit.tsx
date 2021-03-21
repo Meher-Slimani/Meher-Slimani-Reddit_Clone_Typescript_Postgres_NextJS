a2e1e6a159b837491e5297fd46d69f20653d47b5
Meher Slimani <meher.slimeni@gmail.com>
2021-03-21 20:47:46 +0100

    Add search subs route

--- client/src/pages/r/[sub]/submit.tsx
+++ client/src/pages/r/[sub]/submit.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
@@ -84,3 +85,16 @@ const submit = () => {
 };
 
 export default submit;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  try {
+    const cookie = req.headers.cookie;
+    if (!cookie) throw new Error("Missing auth token cookie");
+
+    await axios.get("/auth/me", { headers: { cookie } });
+
+    return { props: {} };
+  } catch (err) {
+    res.writeHead(307, { Location: "/login" }).end();
+  }
+};
